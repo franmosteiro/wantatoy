099e66e2c8c1686b16bc4f9ae882e7dc5a60f805
Alex Epelde <aepelde@gmail.com>
2010-12-16 13:59:37 +0100

    Performance changes (js loading)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function () {
+	$('#title').focus();
+	$('#toy_title').focus(); 
+	$('div.thumb').hover(
+		function () {
+			$(this).find('a.gallery-over').stop().fadeTo('fast', 1);
+			$(this).find('div.box').stop().fadeTo('fast', 1);
+			$(this).find('img').stop().fadeTo('fast', 0.3);
+			},
+		function () {
+			$(this).find('a.gallery-over').stop().fadeTo('fast', 0);
+			$(this).find('div.box').stop().fadeTo('fast', 0);
+			$(this).find('img').stop().fadeTo('fast', 1);
+			}
+		);
+});

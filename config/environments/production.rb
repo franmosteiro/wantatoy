2b98fd0d849aee9b390b8d132a5a3e84022c219b
Alex Epelde <aepelde@gmail.com>
2011-01-31 22:23:27 +0100

    Añadiendo la configuración de Postmark al entorno de producción

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,16 +47,12 @@ Wantatoy::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.smtp_settings = {
-   :address => "smtp.gmail.com",
-   :port => 587,
-   :domain => 'mydomain.com',
-   :user_name => 'thewantatoyteam',
-   :password => 'etcheverry',
-   :authentication => 'plain',
-   :enable_starttls_auto => true
+	  :address => "smtp.postmarkapp.com",
+	  :port => 25,
+	  :authentication => 'plain'
   }
-
+  
   Paperclip.options[:command_path] = "/usr/local/bin"  
 end

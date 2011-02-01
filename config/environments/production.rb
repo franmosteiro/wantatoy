10b5b2f98f74495fd1c5b48bafd2e9a9b64d016a
Fran Mosteiro aka Barakk <framosval@gmail.com>
2011-02-01 15:30:02 +0100

    Actualizar "production.rb". Faltaba cerrar un string

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Wantatoy::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'www.juguetea.org }
+  config.action_mailer.default_url_options = { :host => 'www.juguetea.org' }
   config.action_mailer.smtp_settings = {
 	  :address => "smtp.postmarkapp.com",
 	  :port => 25,
